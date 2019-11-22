e682a3180df961acb2cacaa9c10c8b9e7b4a1876
Courtney Schild <courtneyschild@gmail.com>
2019-11-22 13:41:29 -0800

    remove skip and limit params.

--- src/doctor-lookup-service.js
+++ src/doctor-lookup-service.js
@@ -1,6 +1,6 @@
 export class DoctorLookup {
   async getDoctorByMedicalIssue(medicalIssue) {
-    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=or-portland&user_key=${process.env.API_KEY}`);
     let jsonifiedResponse = await response.json();
     return jsonifiedResponse;
   } catch(error) {
