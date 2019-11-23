c732272db682f8fa44deb4a4f6f146d1808ac0a7
Courtney Schild <courtneyschild@gmail.com>
2019-11-22 16:54:51 -0800

    add functionality to return a list of names on submit; started over.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
-// import
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 // import img from './file.png';
-// import {  } from './file.js';
+import { DoctorLookup } from './doctor-lookup-service.js';
+
+$(document).ready(function() {
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    let medicalIssue = $('#medical-issue').val();
+    $('#medical-issue').val("");
+
+    makeApiCall();
+
+    async function makeApiCall() {
+      let doctorLookup = new DoctorLookup();
+      const response = await doctorLookup.getDoctorByMedicalIssue(medicalIssue);
+      getElements(response);
+    }
+
+    function getElements(response) {
+      $('#output').show();
+      $('.doctor-count').text(response.data.length);
+
+      // QUERY BY MEDICAL ISSUE
+      for (let i = 0; i < response.data.length; i ++) {
+        console.log(response.data);
+        let firstName = response.data[i].profile.first_name;
+        let lastName = response.data[i].profile.last_name;
+        $('#output').append(`<p>${firstName} ${lastName}</p>`);
+      }
+
+    }
+  });
+});
